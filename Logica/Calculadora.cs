868e1e52d6e191ecfbf7500d9fddc63a452bce84
gsusromerovillanueva2024@gmail.com <gsusromerovillanueva2024@gmail.com>
2025-04-21 21:19:57 -0500

    menu y suma

---
+++ Logica/Calculadora.cs
@@ -0,0 +1,7 @@
+namespace appcalculadora2.Logica
+{
+    public class Calculadora
+    {
+        public int Sumar(int n1, int n2)=>n1+n2;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using appcalculadora2.Logica;
+
+Console.WriteLine("SUMAR s, RESTAR r, MULTIPLICAR m, DIVIDIR d");
+Console.WriteLine("Seleccione Opción");
+string opcion=Console.ReadLine();
+Calculadora cal= new();
+Console.WriteLine("Ingrese 2 valores separados por ENTER");
+int n1=int.Parse(Console.ReadLine());
+int n2=int.Parse(Console.ReadLine());
+int res=opcion=="s"?cal.Sumar(n1,n2):0;
+Console.WriteLine("RESULTADO: "+res);
